8bdee7357471b63b7e03812312973c7ee82c3016
Bjørn Vinther <bv@dalux.com>
2020-01-26 18:11:39 +0100

    can now shoot beams

---
+++ Assets/Car ai/Beam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beam : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, 5))
+        {
+            print("hit " + hit.point);
+            Debug.DrawRay(transform.position, hit.point - transform.position, Color.red);
+        } else
+        {
+            print("miss from " + transform.position + " - " + (transform.forward * 5));
+            Debug.DrawRay(transform.position, transform.forward * 5, Color.blue);
+        }
+    }
+}
