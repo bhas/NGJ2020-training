f03cc57927413e0884fa31da01fb03199da6aa36
Bjørn Vinther <g.bvinther@gmail.com>
2020-03-28 16:53:37 +0100

    commit stuff

---
+++ Assets/Car ai/Scripts/Car2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car2 : MonoBehaviour
+{
+    public WheelCollider fl;
+    public WheelCollider fr;
+    public WheelCollider bl;
+    public WheelCollider br;
+    public float motorPower = 30f;
+    public float steerAngle = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dx = Input.GetAxis("Horizontal");
+        var dy = Input.GetAxis("Vertical");
+
+        if (dy != 0)
+        {
+            bl.motorTorque = dy * motorPower;
+            br.motorTorque = dy * motorPower;
+        }
+
+        if (dx != 0)
+        {
+            fl.steerAngle = dx * steerAngle;
+            fr.steerAngle = dx * steerAngle;
+        }
+
+    }
+}
