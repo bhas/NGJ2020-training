38f54759012b5c746113e067e17fbd81cbb9e5f1
Bjørn Vinther <bv@dalux.com>
2020-05-10 15:57:35 +0200

    setup physics for car

--- Assets/Car ai/Scripts/Car2.cs
+++ Assets/Car ai/Scripts/Car2.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class WheelSetup
+{
+    public WheelCollider collider;
+    public Transform model;
+}
+
 public class Car2 : MonoBehaviour
 {
-    public WheelCollider fl;
-    public WheelCollider fr;
-    public WheelCollider bl;
-    public WheelCollider br;
-    public float motorPower = 30f;
-    public float steerAngle = 25f;
+    public float inputX;
+    public float inputY;
+
+    public WheelSetup fl;
+    public WheelSetup fr;
+    public WheelSetup bl;
+    public WheelSetup br;
+    public float maxMoterPower = 30f;
+    public float maxSteeringAngle = 25f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,22 +29,40 @@ public class Car2 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        var dx = Input.GetAxis("Horizontal");
-        var dy = Input.GetAxis("Vertical");
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
 
-        if (dy != 0)
-        {
-            bl.motorTorque = dy * motorPower;
-            br.motorTorque = dy * motorPower;
-        }
+        Steer();
+        Accelerate();
 
-        if (dx != 0)
-        {
-            fl.steerAngle = dx * steerAngle;
-            fr.steerAngle = dx * steerAngle;
-        }
+        // update wheel poses
+        UpdateWheelPose(fr);
+        UpdateWheelPose(fl);
+        UpdateWheelPose(br);
+        UpdateWheelPose(bl);
+    }
 
+
+    private void Steer()
+    {
+        var steerAngle = maxSteeringAngle * inputX;
+        fl.collider.steerAngle = steerAngle;
+        fr.collider.steerAngle = steerAngle;
+    }
+
+    private void Accelerate()
+    {
+        var motorPower = maxMoterPower * inputY;
+        fl.collider.motorTorque = motorPower;
+        fr.collider.motorTorque = motorPower;
+    }
+
+    private void UpdateWheelPose(WheelSetup wheelSetup)
+    {
+        wheelSetup.collider.GetWorldPose(out Vector3 pos, out Quaternion rotation);
+        wheelSetup.model.position = pos;
+        wheelSetup.model.rotation = rotation;
     }
 }
