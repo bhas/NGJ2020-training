b0b8ed2552d0fc0397346156221d14031ac76f00
sm1rt19 <sm1rt19@lortemail.com>
2020-05-22 20:08:09 +0200

    Car can evaluate dots and read surface

--- Assets/Car ai/Scripts/Map2Texture.cs
+++ Assets/Car ai/Scripts/Map2Texture.cs
@@ -7,10 +7,43 @@ using UnityEngine;
 public class Map2Texture : MonoBehaviour
 {
     public const float MAP_SIZE = 300f;
-    public const int GRID_RESOLUTION = 1200;
+    public const int GRID_RESOLUTION = 600;
+
+    public SurfaceType[,] SurfaceArray;
+
+    public static Map2Texture singleton;
+
+
+    public void Awake()
+    {
+        singleton = this;
+    }
+
+    public SurfaceType GetSurface(Vector3 worldPos)
+    {
+        return GetPixel(worldPos);
+    }
+
+    private SurfaceType GetPixel(Vector3 worldPos)
+    {
+        float gridSize = MAP_SIZE / GRID_RESOLUTION;
+        var u = (worldPos.x + (MAP_SIZE / 2f)) / gridSize - 0.5f;
+        var v = (worldPos.z + (MAP_SIZE / 2f)) / gridSize - 0.5f;
+        print(u + ", " + v);
+        try
+        {
+            return SurfaceArray[Mathf.RoundToInt(u), Mathf.RoundToInt(v)];
+        } catch
+        {
+            return SurfaceType.Undefined;
+        }
+        
+    }
 
     public Texture2D CreateTexture2D()
     {
+        SurfaceArray = new SurfaceType[GRID_RESOLUTION, GRID_RESOLUTION];
+
         var texture = new Texture2D(GRID_RESOLUTION, GRID_RESOLUTION, TextureFormat.RGB24, false);
         texture.filterMode = FilterMode.Point;
         texture.wrapMode = TextureWrapMode.Clamp;
@@ -26,8 +59,9 @@ public class Map2Texture : MonoBehaviour
             {
                 var worldPos = GetWorldPos(u, v);
                 var type = RayCast(worldPos);
+                SurfaceArray[u, v] = type;
                 var color = GetColorForSurfaceType(type);
-                texture.SetPixel(u,v, color);
+                texture.SetPixel(u, v, color);
 
                 if (u % 200 == 0 && v % 200 == 0)
                 {
@@ -41,8 +75,8 @@ public class Map2Texture : MonoBehaviour
     {
         float gridSize = MAP_SIZE / GRID_RESOLUTION;
         float x = gridSize * (u + 0.5f) - MAP_SIZE / 2f;
-        float y = gridSize * (v + 0.5f) - MAP_SIZE / 2f;
-        return new Vector3(x,10f,y);
+        float z = gridSize * (v + 0.5f) - MAP_SIZE / 2f;
+        return new Vector3(x, 10f, z);
     }
 
     private Color GetColorForSurfaceType(SurfaceType type)
@@ -92,12 +126,6 @@ public class Map2Texture : MonoBehaviour
         UpdateTexture2D(texture);
         SaveTexture2D(texture, "Assets/Texture.png");
         print("All done");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
 
