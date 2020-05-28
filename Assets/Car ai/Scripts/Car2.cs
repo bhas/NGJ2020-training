3e4747ad40983005b54ffbc9abeae7f73b3c3bbc
sm1rt19 <sm1rt19@lortemail.com>
2020-05-28 20:06:46 +0200

    torqecurve

--- Assets/Car ai/Scripts/Car2.cs
+++ Assets/Car ai/Scripts/Car2.cs
@@ -12,9 +12,26 @@ public class WheelSetup
 
 public class Car2 : MonoBehaviour
 {
+    public AnimationCurve maxMotorTorqueCurve;
 
-    public float maxMotorPower = 300f;
-    public float maxSteeringAngle = 35f;
+    public float maxMotorRPM = 7000f;
+    public float currentMotorRPM;
+
+    public float maxMotorTorque = 400f;
+    public float motorTorqueRate = 100f;
+    public float currentMaxMotorTorque;
+    public float currentMotorTorque;
+    public float targetMotorTorque;
+
+    public float maxSteeringAngle = 40f;
+    public float steeringAngleRate = 50f;
+    public float currentSteeringAngle;
+
+    public float frontWheelRPMLeft;
+    public float frontWheelRPMRight;
+    public float rearWheelRPMLeft;
+    public float rearWheelRPMRight;
+
     private float inputX;
     private float inputY;
 
@@ -44,6 +61,11 @@ public class Car2 : MonoBehaviour
         UpdateWheelPose(fl);
         UpdateWheelPose(br);
         UpdateWheelPose(bl);
+
+        frontWheelRPMLeft = fl.collider.rpm;
+        frontWheelRPMRight = fr.collider.rpm;
+        rearWheelRPMLeft = bl.collider.rpm;
+        rearWheelRPMRight = br.collider.rpm;
     }
 
 
@@ -56,11 +78,31 @@ public class Car2 : MonoBehaviour
 
     private void Accelerate()
     {
-        var motorPower = maxMotorPower * inputY;
-        fl.collider.motorTorque = motorPower;
-        fr.collider.motorTorque = motorPower;
-        bl.collider.motorTorque = motorPower;
-        br.collider.motorTorque = motorPower;
+        currentMotorRPM = (bl.collider.rpm + br.collider.rpm) / 2f * 10f;
+        currentMaxMotorTorque = maxMotorTorqueCurve.Evaluate(currentMotorRPM / maxMotorRPM) * maxMotorTorque;
+        targetMotorTorque = currentMaxMotorTorque * inputY;
+        //float targetMotorTorque = maxMotorTorque * inputY;
+        float deltaMotorTorque = motorTorqueRate * Time.deltaTime;
+        currentMotorTorque = Step(currentMotorTorque, targetMotorTorque, deltaMotorTorque);
+        //fl.collider.motorTorque = currentMotorTorque;
+        //fr.collider.motorTorque = currentMotorTorque;
+        bl.collider.motorTorque = currentMotorTorque;
+        br.collider.motorTorque = currentMotorTorque;
+    }
+
+    private static float Step(float currentValue, float targetValue, float delta)
+    {
+        if (targetValue > currentValue)
+        {
+            delta = Mathf.Min(delta, targetValue - currentValue);
+            currentValue += delta;
+        }
+        if (targetValue < currentValue)
+        {
+            delta = Mathf.Min(delta, currentValue - targetValue);
+            currentValue -= delta;
+        }
+        return currentValue;
     }
 
     private void UpdateWheelPose(WheelSetup wheelSetup)
